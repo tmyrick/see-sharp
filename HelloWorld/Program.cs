d53d8aca5e17a1f1e573a88841612bbad128a45e
tmyrick <travis.myrick@dutchie.com>
2022-12-29 08:59:07 -0800

    sections 1 thru 3

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,91 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        // private int number;
+        // private int Number = 1;
+
+        private const int MyNum = 255;
+        private const string Name = "Trav";
+
+
+        static void Main(string[] args)
+        {
+            // byte number = 2;
+            // int count = 10;
+            // var totalPrice = 20.95f;
+            // char character = 'A';
+            // string firstName = "Trav";
+            // bool isWorking = true;
+            //
+            // // Console.WriteLine("The number is {0}, name is {1}", number, Name);
+            //
+            // Console.WriteLine(count);
+            // Console.WriteLine(totalPrice);
+            // Console.WriteLine(character);
+            // Console.WriteLine(firstName);
+
+            // Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            // Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+            // const float Pi = 3.14f;
+
+            // byte b = 1;
+            // int i = b;
+            // Console.WriteLine(b);
+            // Console.WriteLine(i);
+
+            // int i = 1;
+            // byte b = (byte)i;
+            //
+            // Console.WriteLine(b);
+            // Console.WriteLine(i);
+
+            // string s = "11";
+            // int i = Convert.ToInt32(s);
+            // int j = int.Parse(s);
+            //
+            // Console.WriteLine(i);
+            // Console.WriteLine(j);
+
+            // int i = 1000;
+            // byte b = (byte)i;
+            // Console.WriteLine(b);
+
+            // try
+            // {
+            //     string str = "true";
+            //     bool b = Convert.ToBoolean(str);
+            //     Console.WriteLine(b);
+            //     
+            //     var number = "1234";
+            //     byte i = Convert.ToByte(number);
+            //     Console.WriteLine(i);
+            // }
+            // catch (Exception e)
+            // {
+            //     Console.WriteLine("The number could not be converted to a byte.");
+            // }
+
+            // var a = 10;
+            // var b = 2;
+            // var c = 3;
+            // Console.WriteLine(a + b * c);
+
+            // byte number = 1000;
+            // Console.WriteLine(number);
+            
+            // byte number = 255;
+            // number += 2;
+            // Console.WriteLine(number);
+            
+            // var z = true && false;
+            // Console.WriteLine(z);
+            
+            
+        }
+    }
+}
